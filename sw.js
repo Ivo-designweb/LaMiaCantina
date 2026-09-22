8b0cfef6d8d4bddba49cf33e33600374e119ad83
Ivo-designweb <ivotaffarel@gmail.com>
2026-09-22 23:24:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    il refresh dell'app su tutti i dispositivi.
 ═══════════════════════════════════════════════════ */
 
-const CACHE_VERSION = 'cantina-v8.2';
+const CACHE_VERSION = 'cantina-v8.3';
 const ASSETS = [
   './index.html',
   './manifest.json',
